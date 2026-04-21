876605f28be2e47a0e9ce6a6a38f117c2a28ec6f
tiaoedi <tiaoedi@outlook.com>
2026-04-21 07:27:48 -0300

    move colresize-toggle script to nix package

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -2,7 +2,19 @@
 , inputs
 , username
 , ...
-}: {
+}: let
+  colresizeToggle = pkgs.writeShellScriptBin "colresize-toggle" ''
+    CURRENT=$(hyprctl activewindow -j | jq '.size[0]')
+    MONITOR=$(hyprctl monitors -j | jq '.[0].width')
+    THRESHOLD=$(echo "$MONITOR * 0.9" | bc | cut -d. -f1)
+    if [ "$CURRENT" -ge "$THRESHOLD" ]; then
+      hyprctl dispatch layoutmsg "colresize -0.5"
+    else
+      hyprctl dispatch layoutmsg "colresize +0.5"
+    fi
+  '';
+in {
+  home.packages = [ colresizeToggle ];
   wayland.windowManager.hyprland = {
     enable = true;
 
@@ -136,7 +148,7 @@
           "$mainMod, Q, killactive"
           "$mainMod SHIFT, E, exit"
           # "$mainMod, F, exec, ~/Meu-Nix/modules/home/scripts/colresize-toggle.sh"
-          "$mainMod, F, exec, ~/.config/hypr/scripts/colresize-toggle.sh"
+          "$mainMod, F, exec, colresize-toggle"
           "$mainMod SHIFT, F, fullscreen"
           "$mainMod, V, togglefloating"
 
