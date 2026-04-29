1756b9ed30723d6cd63d200893e9077fe840b60d
tiaoedi <tiaoedi@outlook.com>
2026-04-29 20:20:08 -0300

    nixos update: 2026-04-29

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -1,12 +1,11 @@
-{
-  inputs,
-  pkgs,
-  ...
+{ inputs
+, pkgs
+, ...
 }: {
-  imports = [inputs.noctalia.homeModules.default];
-
+  imports = [ inputs.noctalia.homeModules.default ];
   programs.noctalia-shell = {
     enable = true;
-    settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+    settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
+    colors = builtins.fromJSON (builtins.readFile ./colors.json);
   };
 }
