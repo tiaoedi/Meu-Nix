5ed6bd7a43e9b6b64eeda50aadd82ccce7867dc3
tiaoedi <tiaoedi@outlook.com>
2026-05-15 01:11:33 -0300

    use hyprland.lua via xdg.configFile

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -11,23 +11,6 @@
   '';
 in {
   home.packages = [ colresizeToggle ];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      monitor = "HDMI-A-1,1920x1080@60,0x0,1";
-      "$mainMod" = "SUPER";
-      exec-once = [
-        "noctalia-shell"
-        "qs -c overview"
-        "nm-applet --indicator"
-        "wl-paste --watch cliphist store"
-      ];
-      bind = [
-        "$mainMod, Q, killactive"
-        "$mainMod SHIFT, E, exit"
-        "$mainMod, Return, exec, ghostty"
-      ];
-    };
-  };
+  wayland.windowManager.hyprland.enable = true;
+  xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
 }
